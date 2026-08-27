dc3ef74c68e8b6ed7f2e85fc0abc6262e94634c1
Ifrim Alexandra <aleifrim79@gmail.com>
2026-08-27 19:59:14 +0300

    changed load into safeLoad

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->safeLoad();
 
 function getDatabase(): MongoDB\Database
 {
